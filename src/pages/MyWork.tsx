26f37a6b531790e1e2fb0cec7936d35c2ebc2529
Gururagavendran <gururagavendran52@gmail.com>
2026-04-22 02:25:22 +0530

    Removed Experience from my work

--- src/pages/MyWork.tsx
+++ src/pages/MyWork.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import ProjectsSection from "@/components/ProjectsSection";
 import PublicationsSection from "@/components/PublicationsSection";
-import ExperienceSection from "@/components/ExperienceSection";
 
 const MyWork = () => {
   return (
